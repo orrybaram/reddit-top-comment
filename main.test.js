23eba3a7f152e0842d8718218ddc5325d5149b2c
Orry Baram <orrybaram@gmail.com>
2015-12-27 13:47:18 -0500

    added some dom tests

--- main.test.js
+++ main.test.js
@@ -1,5 +1,13 @@
-describe("Hover Intent", function() {
-	it("will be true", function() {
-		expect(true).to.equal(true);	
+describe("htmlDecode", function() {
+	it("will take escaped html and return as unescaped html", function() {
+		expect(htmlDecode("&lt;div class=&quot;thing&quot;&gt;&lt;/div&gt;")).to.equal('<div class="thing"></div>');	
+	})
+})
+
+describe("addTargetBlankToLinks", function() {
+	it("will find all links in an element and add a target blank attribute to them", function() {
+		$linkTest = document.getElementById("link-test");
+		addTargetBlankToLinks($linkTest);
+		expect($linkTest.getElementsByTagName('a')[0].getAttribute("target")).to.equal("_blank");
 	})
 })
